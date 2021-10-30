9f16452ab629699255ed13f5ec3d60bf4dda8ab7
Ulysses Ortega <naylher@gmail.com>
2021-10-30 11:11:22 +0100

    commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", e => {
+    console.log("SW Instalado con exito");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./assets/css/style.css", "./assets/img/pwa-icons/logo192.png"]);
+        })
+    )
+});
+ 
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(response => {
+            
+            console.log(`Intercepting fetch request for: ${e.request.url}`);
+            /* return response || fetch(e.request); */
+
+            if (response) {
+                //recupera de cache
+                return response;
+            }
+            // recuperar de la peticion a la url
+            return fetch(e.request);
+        })
+        )
+    })
