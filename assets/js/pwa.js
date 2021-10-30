962d6219249586f11561038d1232e74d0ab47be5
Ulysses Ortega <naylher@gmail.com>
2021-10-30 13:03:48 +0100

    Update pwa.js

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -1,8 +1,8 @@
 if("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./sw.js")
         .then(registration => {
-            console.log("SW Registered!");
-            console.log(registration);
+            /* console.log("SW Registered!");
+            console.log(registration); */
         }).catch(error => {
             console.log("SW Registration Failed!");
             console.log((error));
