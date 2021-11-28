bba48dd62f9e32b73d6ce667bd4033d5faf92921
Ulysses Ortega <naylher@gmail.com>
2021-11-28 10:27:03 +0000

    Create pdf.js

---
+++ assets/js/pdf.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    document.getElementById("exportFacturaPDF")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("facturaPDF");
+            console.log(facturaPDF);
+            console.log(window);
+            var opt = {
+                margin: 0.5,
+                filename: 'miFactura.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
